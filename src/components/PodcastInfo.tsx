d075bc4d531a5600735e573d84440f90bcf0d3fc
Xavier Roig Roca <xroig@albira.com>
2023-01-09 18:57:51 +0100

    link added to podcast info

--- src/components/PodcastInfo.tsx
+++ src/components/PodcastInfo.tsx
@@ -1,5 +1,5 @@
 import i18n from "../i18n/index";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { usePodcast } from "../hooks/usePodcast";
 import styles from "../stylesheets/PodcastInfo.module.css";
 
@@ -11,14 +11,18 @@ const PodcastInfo = () => {
   return(
     <div>
       <div className={'box-shadow ' + styles.podcastCard}>
-        <img 
-          className={styles.podcastImage} 
-          src={podcast.image} 
-          alt={`Podcast ${podcast.title}`} 
-        />
+        <Link to={"/podcast/" + podcastId}>
+          <img 
+            className={styles.podcastImage} 
+            src={podcast.image} 
+            alt={`Podcast ${podcast.title}`} 
+          />
+        </Link>
         <hr/>
-        <h2 className={styles.podcastTitle}>{podcast.title}</h2>
-        <p className={styles.podcastAuthor}><i>{i18n.PODCAST.PODCAST_AUTHOR + ' ' + podcast.author}</i></p>
+        <Link to={"/podcast/" + podcastId}>
+          <h2 className={styles.podcastTitle}>{podcast.title}</h2>
+          <p className={styles.podcastAuthor}><i>{i18n.PODCAST.PODCAST_AUTHOR + ' ' + podcast.author}</i></p>
+        </Link>
         <hr/>
         <p className={styles.podcastDescriptionTitle}><b>{i18n.PODCAST.DESCRIPTION}</b></p>
         <p className={styles.podcastDescription}><i>{podcast.summary}</i></p>
